fffb5c784ff93a537606eb8fe859462a072da41f
Yezhikang <794101456@qq.com>
2020-03-12 16:18:03 +0800

    房间内页制作

---
+++ airbnb/src/components/Rooms/RoomsHeader.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "RoomsHeader"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ airbnb/src/views/TheRooms.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "TheRooms"
+    }
+</script>
+
+<style scoped>
+
+</style>
